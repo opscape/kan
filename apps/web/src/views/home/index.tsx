b782541025a694b3d538e2db97f90443096d20cd
Sean Chen <xuchenx@gmail.com>
2026-07-28 09:00:52 -0700

    update logo

--- apps/web/src/views/home/index.tsx
+++ apps/web/src/views/home/index.tsx
@@ -1,11 +1,7 @@
 import Image from "next/image";
-import Link from "next/link";
 import { t } from "@lingui/core/macro";
-import { Trans } from "@lingui/react/macro";
 import { useTheme } from "next-themes";
-import { IoLogoGithub } from "react-icons/io";
 
-import Button from "~/components/Button";
 import { PageHead } from "~/components/PageHead";
 import Cta from "./components/Cta";
 import FAQs from "./components/Faqs";
@@ -19,70 +15,16 @@ export default function HomeView() {
 
   return (
     <Layout>
-      <PageHead title="Kan.bn | The open source alternative to Trello" />
+      <PageHead title="Opscape | The open source alternative to Trello" />
       <div className="flex h-full w-full flex-col lg:pt-[5rem]">
         <div className="w-full pb-10 pt-32 lg:py-32">
           <div className="my-10 flex h-full w-full animate-fade-down flex-col items-center justify-center px-4">
-            <div className="flex items-center gap-2">
-              <div className="relative animate-fade-in overflow-hidden rounded-full bg-gradient-to-b from-light-300 to-light-400 p-[2px] dark:from-dark-300 dark:to-dark-400">
-                <div className="gradient-border absolute inset-0 animate-border-spin" />
-
-                <div className="relative z-10 rounded-full bg-light-50 dark:bg-dark-50">
-                  <Link
-                    href="https://github.com/kanbn/kan"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    className="flex items-center gap-2 px-4 py-1 text-center text-xs text-light-1000 dark:text-dark-1000 lg:text-sm"
-                  >
-                    {t`Star on Github`}
-                    <IoLogoGithub size={20} />
-                  </Link>
-                </div>
-              </div>
-
-              {/* <div className="relative overflow-hidden rounded-full bg-gradient-to-b from-light-300 to-light-400 p-[2px] dark:from-dark-300 dark:to-dark-400">
-                <div className="relative z-10 rounded-full bg-light-50 dark:bg-dark-50">
-                  <Link
-                    href="https://news.ycombinator.com/item?id=44157177"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    className="flex items-center gap-2 px-4 py-1 text-center text-xs text-light-1000 dark:text-dark-1000 lg:text-sm"
-                  >
-                    {t`#1 Hacker News`}
-                    <div className="relative">
-                      <div className="absolute inset-1 bg-white" />
-                      <IoLogoHackernews
-                        size={20}
-                        className="relative text-orange-500"
-                      />
-                    </div>
-                  </Link>
-                </div>
-              </div> */}
-            </div>
-
             <p className="mt-2 text-center text-4xl font-bold text-light-1000 dark:text-dark-1000 lg:text-5xl">
-              <Trans>
-                The open source <br /> alternative to Trello
-              </Trans>
+              Opscape Kan
             </p>
 
             <p className="text-md mt-3 max-w-[450px] text-center text-light-950 dark:text-dark-900 lg:max-w-[600px] lg:text-lg">
               {t`A powerful, flexible kanban app that helps you organise work, track progress, and deliver results—all in one place.`}
-            </p>
-
-            <div className="mt-6 flex gap-2">
-              <Button href="/signup">{t`Get started on Cloud`}</Button>
-              <Button
-                variant="secondary"
-                href="https://github.com/kanbn/kan"
-                openInNewTab
-              >
-                {t`Self host with Github`}
-              </Button>
-            </div>
-            <p className="mt-4 text-center text-sm text-light-950 dark:text-dark-900">
-              {t`No credit card required`}
             </p>
           </div>
         </div>
--- packages/email/src/templates/mention.tsx
+++ packages/email/src/templates/mention.tsx
@@ -46,7 +46,7 @@ export const MentionTemplate = ({
             color: "#232323",
           }}
         >
-          kan.bn
+          Opscape
         </Heading>
         <Heading
           style={{ fontSize: "24px", fontWeight: "bold", color: "#232323" }}
